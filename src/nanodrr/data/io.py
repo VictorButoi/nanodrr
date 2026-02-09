4bb41e1abb228e8ee39228db9d9c277c01abf9a4
Vivek Gopalakrishnan <vivekg@mit.edu>
2026-02-09 12:23:06 -0500

    Add constructor from torchio Images (#13)
    * Add constructor from Images
    
    * Fuse world to grid transform
    
    * Save the original affine
    
    * Update README.md

--- src/nanodrr/data/io.py
+++ src/nanodrr/data/io.py
@@ -10,6 +10,7 @@ class Subject(torch.nn.Module):
         self,
         imagedata: Float[torch.Tensor, "1 1 D H W"],
         labeldata: Float[torch.Tensor, "1 1 D H W"],
+        voxel_to_world: Float[torch.Tensor, "4 4"],
         world_to_voxel: Float[torch.Tensor, "4 4"],
         voxel_to_grid: Float[torch.Tensor, "4 4"],
         isocenter: Float[torch.Tensor, "3"],
@@ -17,11 +18,14 @@ class Subject(torch.nn.Module):
         super().__init__()
         self.register_buffer("image", imagedata)
         self.register_buffer("label", labeldata)
-        self.register_buffer("world_to_voxel", world_to_voxel)
-        self.register_buffer("voxel_to_grid", voxel_to_grid)
+        self.register_buffer("world_to_grid", voxel_to_grid @ world_to_voxel)
         self.register_buffer("isocenter", isocenter)
 
-        self.n_classes: int = int(self.label.max().item()) + 1
+        self.register_buffer("voxel_to_world", voxel_to_world)
+        self.register_buffer("world_to_voxel", world_to_voxel)
+        self.register_buffer("voxel_to_grid", voxel_to_grid)
+
+        self.n_classes = int(self.label.max().item()) + 1
 
     @classmethod
     def from_filepath(
@@ -30,12 +34,23 @@ class Subject(torch.nn.Module):
         labelpath: str | Path | None = None,
         convert_to_mu: bool = True,
         mu_water: float = 0.019,
-    ) -> Subject:
+    ):
         image = ScalarImage(imagepath)
         label = LabelMap(labelpath) if labelpath is not None else None
+        return cls.from_images(image, label, convert_to_mu, mu_water)
 
-        # Load the affine matrices
-        world_to_voxel = torch.linalg.inv(torch.from_numpy(image.affine)).to(dtype=torch.float32)
+    @classmethod
+    def from_images(
+        cls,
+        image: ScalarImage,
+        label: LabelMap | None = None,
+        convert_to_mu: bool = True,
+        mu_water: float = 0.019,
+    ):
+        # Load the affine matrix
+        voxel_to_world = torch.from_numpy(image.affine)
+        world_to_voxel = voxel_to_world.inverse().to(dtype=torch.float32)
+        voxel_to_world = voxel_to_world.to(dtype=torch.float32)
 
         # Load the image data
         imagedata = cls.fixdim(image.data)
@@ -53,12 +68,10 @@ class Subject(torch.nn.Module):
         dims = torch.tensor([W - 1, H - 1, D - 1], dtype=torch.float32)
         scale = 2.0 / dims
         voxel_to_grid = torch.eye(4, dtype=torch.float32)
-        voxel_to_grid[0, 0] = scale[0]
-        voxel_to_grid[1, 1] = scale[1]
-        voxel_to_grid[2, 2] = scale[2]
+        voxel_to_grid[:3, :3] = torch.diag(scale)
         voxel_to_grid[:3, 3] = -1.0
 
-        return cls(imagedata, labeldata, world_to_voxel, voxel_to_grid, isocenter)
+        return cls(imagedata, labeldata, voxel_to_world, world_to_voxel, voxel_to_grid, isocenter)
 
     @staticmethod
     def hu_to_mu(data: Float[torch.Tensor, "1 1 D H W"], mu_water: float) -> Float[torch.Tensor, "1 1 D H W"]:
